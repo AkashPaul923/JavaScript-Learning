5fd91d600929235216ff24efadfea5f98019f3dc
Akash Paul <152735946+AkashPaul923@users.noreply.github.com>
2024-08-29 23:57:47 +0600

    if else multiple condition

---
+++ if-else-multiple-condition.js
@@ -0,0 +1,21 @@
+// And condition
+// -----------------------------------------
+var hight = 68;
+var weight = 70;
+if(hight >=70 && weight <=75){
+    console.log("Qualify");
+}
+else{
+    console.log("Disqualify");
+}
+
+// OR condition
+// -------------------------------------------
+var salary = 25000;
+var asset = 100000;
+if(salary >= 25000 || asset >= 200000){
+    console.log("Loan Granted");
+}
+else{
+    console.log("Loan does not Granted ");
+}
