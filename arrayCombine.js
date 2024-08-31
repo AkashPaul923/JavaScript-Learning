a7fc2b395b83ddd82236f41e187edeaf13033ba9
Akash Paul <152735946+AkashPaul923@users.noreply.github.com>
2024-08-31 14:39:45 +0600

    combine two array

---
+++ arrayCombine.js
@@ -0,0 +1,14 @@
+/**Combining Arrays
+Instructions:
+
+Create two arrays of your choice.
+Use the concat method to combine the two arrays into a new array.
+Print both the original arrays and the combined array using console.log().
+ */
+
+var array1 = ["mango", "banana", "lichi", "watermelon"]
+var array2 = [1, 2, 3]
+
+var array3 =array1.concat(array2)
+
+console.log((array3))
