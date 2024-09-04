20ebfea6190a680a7df1512acb59208b12a93272
Akash Paul <152735946+AkashPaul923@users.noreply.github.com>
2024-09-05 00:40:34 +0600

    concatination an array

---
+++ concateArray.js
@@ -0,0 +1,15 @@
+/**
+ * Use a for...of loop to concatenate all the elements of an array into a single string.
+
+Input: var numbers = ['Tom', 'Tim', 'Tin', 'Tik']
+
+Output:
+
+'TomTimTinTik'
+*/ 
+
+var arr = ['Tom', 'Tim', 'Tin', 'Tik']
+
+var str = arr.join("")
+
+console.log(str)
